75c8f3e155a7493d78c7306a96f5634826bbf65b
Nico1905 <leidenfrost.nico@gmail.com>
2017-05-15 18:36:09 +0200

    fixed js file loading problems

--- public/index.js
+++ public/index.js
@@ -1,60 +1,66 @@
-var socket = io();
-var secret = false;
+$(document).ready(function () {
 
-function show_snackbar(text) {
-    $('#snackbar').text(text);
-    $('#snackbar').addClass('show');
-    setTimeout(function(){ $('#snackbar').removeClass('show'); }, 3000);
-}
+    $('#myModal').modal('show');
 
-$('#send_button').click(function() {
-    if ($('#m').val() != '') {
-        socket.emit('chat message', $('#m').val());
-        $('#m').val('');
+
+    var socket = io();
+    var secret = false;
+
+    function show_snackbar(text) {
+        $('#snackbar').text(text);
+        $('#snackbar').addClass('show');
+        setTimeout(function(){ $('#snackbar').removeClass('show'); }, 3000);
+    }
+
+    $('#send_button').click(function() {
+        if ($('#m').val() != '') {
+            socket.emit('chat message', $('#m').val());
+            $('#m').val('');
+            return false;
+        }
         return false;
-    }
-    return false;
-});
+    });
 
-$('.card').on('click', function() {
-    $(this).addClass('flipped');
-    $(this).children('.icon').attr('src', 'tombstone.svg');
-    $(this).children('.icon-small').remove();
-    if ($(this).children('p').text() != '_') {
-        show_snackbar($(this).children('p').text() + ' died');
-    }
-    $(this).children('p').text('_');
-});
+    $('.card').on('click', function() {
+        $(this).addClass('flipped');
+        $(this).children('.icon').attr('src', 'tombstone.svg');
+        $(this).children('.icon-small').remove();
+        if ($(this).children('p').text() != '_') {
+            show_snackbar($(this).children('p').text() + ' died');
+        }
+        $(this).children('p').text('_');
+    });
 
-$('#join_room').change(function() {
-    secret = !secret;
-    socket.emit('join secret', secret);
-});
+    $('#join_room').change(function() {
+        secret = !secret;
+        socket.emit('join secret', secret);
+    });
 
-socket.on('chat message', function(msg) {
-    var now = new Date(Date.now());
-    var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-    $('#messages').append('<li class="message-item"><span class="gicon"></span><span class="nickname">Nico</span><span class="timestamp">' + formatted + '</span><p class="message">' + msg + '</p></li>');
-    $('.messages-wrapper').scrollTop($('.messages-wrapper').prop('scrollHeight'));
-    if (msg == 'show info') {
-        show_snackbar('The city i don\'t know ');
-    }
-    if (msg == 'night') {
-        $('.card').each(function() {
-            if ($(this).children('p').text() != '_') {
-                $(this).children('.icon').attr('src', 'werewolf.svg');
-            }
-        });
-    }
-    if (msg == 'day') {
-        $('.card').each(function() {
-            if ($(this).children('p').text() != '_') {
-                $(this).children('.icon').attr('src', 'villager.svg');
-            }
-        });
-    }
-});
+    socket.on('chat message', function(msg) {
+        var now = new Date(Date.now());
+        var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+        $('#messages').append('<li class="message-item"><span class="gicon"></span><span class="nickname">Nico</span><span class="timestamp">' + formatted + '</span><p class="message">' + msg + '</p></li>');
+        $('.messages-wrapper').scrollTop($('.messages-wrapper').prop('scrollHeight'));
+        if (msg == 'show info') {
+            show_snackbar('The city i don\'t know is ');
+        }
+        if (msg == 'night') {
+            $('.card').each(function() {
+                if ($(this).children('p').text() != '_') {
+                    $(this).children('.icon').attr('src', 'werewolf.svg');
+                }
+            });
+        }
+        if (msg == 'day') {
+            $('.card').each(function() {
+                if ($(this).children('p').text() != '_') {
+                    $(this).children('.icon').attr('src', 'villager.svg');
+                }
+            });
+        }
+    });
 
-socket.on('logg', function(obj) {
-    console.log(obj);
+    socket.on('logg', function(obj) {
+        console.log(obj);
+    });
 });
