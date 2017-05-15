8036a9936daa7b6763de84fbb32113814ada8cf3
chrisizeh <zeh.chrisi@gmail.com>
2017-05-15 20:51:41 +0200

    added start game and werewolf assignment

--- server.js
+++ server.js
@@ -6,36 +6,73 @@ var io = require('socket.io')(http);
 var werewolf = io.of('werewolf')
 
 var running = false;
+var werewolfList = new Array();
 
 app.use(express.static('public'));
 
-io.on('connection', function(socket) {
-    console.log('a user connected');
-    if(running)
-        socket.join('spectator');
+function waitForList(list, count) {
+    if (list.length < count) {
+        setTimeout(waitForList, 500, list, count); // setTimeout(func, timeMS, params...)
+        console.log('wait...');
+    } else {
+        console.log(werewolfList);
+        werewolf.emit('send werewolfs', werewolfList);
+    }
+}
 
-    socket.on('user_name', function(name){
+io.on('connection', function(socket) {
+    if(running){
+        io.emit('chat message', 'A new Ghost is spectating you!');
+        socket.join('spectator');
+        console.log('a spectator connected');
+    }
+    else{
+        console.log('a user connected');
+        socket.join('farmer');
+    }
+
+    socket.on('set username', function(name){
         if(!running)
-            io.to('main').emit('chat message', name, 'Hello I ams!');
+            io.emit('chat message', 'Hello I ams!');
     });
 
     socket.on('start', function(){
+        console.log('Game has started');
         running = true;
+
+        io.emit('chat message', 'Let the Games begin!!');     
+        io.clients(function(error, clients){
+            if (error) throw error;
+            console.log(clients); // => [PZDoMHjiu8PYfRiKAAAF, Anw2LatarvGVVXEIAAAD]
+            var count = Math.floor(Math.random() * clients.length/2) + 2;
+            console.log(count);
+            for (var i = count;i > 0; i--) {
+                var client = clients.splice(clients.length * Math.random() | 0, 1)[0]
+                console.log(client);
+                socket.to(client).emit('chat message', 'You are a werewolf!');
+                socket.to(client).emit('werewolf');
+            }
+
+            io.emit('start game', clients.length);
+            waitForList(werewolfList, count);
+        });
+
+        
     })
 
     socket.on('chat message', function(msg, night) {
-        if (!night)
+        console.log(msg);
+        if (!night){
             io.emit('chat message', msg);
+            console.log('day');
+        }
         else
             werewolf.emit('chat message', msg);
     });
 
-    socket.on('join werewolf', function(enter) {
-        if (enter) {
-            socket.join('werewolf');
-        } else {
-            socket.leave('werewolf');
-        }
+    socket.on('join werewolf', function(name) {
+        socket.join('werewolf');
+        werewolfList.push(name);
     });
 
     socket.on('disconnect', function() {
