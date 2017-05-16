2f20c9433df5e276fa6fa07e5ca720ebf50b1838
Nico1905 <leidenfrost.nico@gmail.com>
2017-05-16 14:28:23 +0200

    updated...

--- public/index.js
+++ public/index.js
@@ -20,14 +20,6 @@ $(document).ready(function () {
         socket.emit('set username', $('#username-input').val());
     });
 
-    socket.on('checked username', function(valid) {
-        if (valid) {
-            $('#join_modal').modal("hide");
-        } else {
-            $('.join-error').removeClass('blank');
-        }
-    });
-
     $('#send_button').on('click', function() {
         var msg = $('#m').val();
         if (msg != '') {
@@ -82,7 +74,33 @@ $(document).ready(function () {
         $('.messages-wrapper').scrollTop($('.messages-wrapper').prop('scrollHeight'));
     });
 
-    socket.on('werewolf', function(obj) {
+    socket.on('checked username', function(valid) {
+        if (valid) {
+            $('#join_modal').modal("hide");
+        } else {
+            $('.join-error').removeClass('blank');
+        }
+    });
+
+    socket.on('werewolf', function() {
         socket.emit('join werewolf');
+    });
+
+    socket.on('story message', function(msg) {
+        var now = new Date(Date.now());
+        var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+        $('#messages').append('<li class="message-item"><span class="sicon"></span><span class="nickname">' + name + '</span><span class="timestamp">' + formatted + '</span><p class="message">' + msg + '</p></li>');
+    });
+
+    socket.on('snackbar message', function(msg) {
+        console.log('sb msg');
+        show_snackbar(msg);
+    });
+
+    socket.on('start game', function(users) {
+        console.log('start');
+        for (var i = 0; i < users.length; i++) {
+            $('#card-container').append('<div class="card"><img src="villager.svg" class="icon"><p class="card-username">' + users[i] + '</p></div>');
+        }
     });
 });
